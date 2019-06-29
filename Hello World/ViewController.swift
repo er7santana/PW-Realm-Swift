2cd7c6cdea9d8012bbc4464e456c9358f7930990
TMAN <tmantsbr@TMANs-Mac-mini.local>
2019-06-28 23:39:22 -0300

    Initial Commit

---
+++ Hello World/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by TMAN on 28/06/19.
+//  Copyright © 2019 TMAN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
